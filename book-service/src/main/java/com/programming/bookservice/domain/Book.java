ec7c01253990cb916fe85f4bc1ded22e563ab4ab
parvin <pr.valadi@gmail.com>
2024-05-08 09:36:26 +0330

    p.valadi upload file

--- book-service/src/main/java/com/programming/bookservice/domain/Book.java
+++ book-service/src/main/java/com/programming/bookservice/domain/Book.java
@@ -24,7 +24,7 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "NAME", nullable = false)
+    @Column(name = "NAME",unique = true, nullable = false)
     private String name;
 
     @Column(name = "PUBLISHER_NAME")
