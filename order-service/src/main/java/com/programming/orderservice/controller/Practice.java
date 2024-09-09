a4d137e41b5960ec4075820d5556cbbda782a4ac
parvin <pr.valadi@gmail.com>
2024-09-09 13:43:16 +0330

    p.valadi practice: user can call api just two times per hour

--- order-service/src/main/java/com/programming/orderservice/controller/Practice.java
+++ order-service/src/main/java/com/programming/orderservice/controller/Practice.java
@@ -1,5 +1,6 @@
 package com.programming.orderservice.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Practice {
 
+    @RateLimiter(name = "apiRateLimiter", fallbackMethod = "rateLimitFallback")
     @GetMapping(value = "/practice")
     public ResponseEntity practice(){
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    public ResponseEntity rateLimitFallback(Throwable t) {
+        return new ResponseEntity(HttpStatus.TOO_MANY_REQUESTS);
     }
 }
